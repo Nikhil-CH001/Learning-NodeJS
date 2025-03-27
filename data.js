64a19a79674e9b3146e8c89d525d497aea04ceb9
Nikhil-CH001 <nikhilchy001@gmail.com>
2025-03-27 20:51:00 +0545

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        name:"Nikhil",
+        age:20
+    },
+    {
+        name:"Sumit",
+        age:20
+    },
+    {
+        name:"Sudip",
+        age:20
+    },
+    {
+        name:"Nishanta",
+        age:20
+    }
+]
+
+module.exports = data
